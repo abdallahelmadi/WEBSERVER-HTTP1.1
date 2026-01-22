39503bdb13e90d059120657a9b592eb823230763
aadouay <synacyb@gmail.com>
2026-01-22 12:31:45 +0100

    delete

--- .server/main/.cpp
+++
@@ -1,8 +0,0 @@
-#include <time.hpp>
-int parse(int ac, char* av[]);
-int run(long long start);
-
-int main(int ac, char *av[]) {
-  long long c = time::clock();
-  int r = parse(ac, av); return r == 2 ? 0 : (r || run(c));
-}
